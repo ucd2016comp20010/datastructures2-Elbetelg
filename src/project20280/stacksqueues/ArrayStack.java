ced083ed19f50f2f9d5b44bcbb6121619118580d
elbetelg <elbetelgetu2020@gmail.com>
2026-02-04 21:58:36 +0000

    Completed ArrayStack implementation for Q1 a.

--- src/project20280/stacksqueues/ArrayStack.java
+++ src/project20280/stacksqueues/ArrayStack.java
@@ -17,7 +17,7 @@ public class ArrayStack<E> implements Stack<E> {
     /**
      * Index of the top element of the stack in the array.
      */
-    private final int t = -1;                      // index of the top element in stack
+    private int t = -1;                      // index of the top element in stack
 
     /**
      * Constructs an empty stack using the default array capacity.
@@ -34,6 +34,7 @@ public class ArrayStack<E> implements Stack<E> {
     @SuppressWarnings({"unchecked"})
     public ArrayStack(int capacity) {        // constructs stack with given capacity
         // TODO
+        data = (E[]) new Object[capacity];
     }
 
     /**
@@ -65,6 +66,12 @@ public class ArrayStack<E> implements Stack<E> {
     @Override
     public void push(E e) {
         // TODO
+       if(size() == data.length){
+           throw new IllegalStateException();
+       }
+       t++;
+       data[t] = e;
+
     }
 
     /**
@@ -75,7 +82,10 @@ public class ArrayStack<E> implements Stack<E> {
     @Override
     public E top() {
         // TODO
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        return data[t];
     }
 
     /**
@@ -86,7 +96,14 @@ public class ArrayStack<E> implements Stack<E> {
     @Override
     public E pop() {
         // TODO
-        return null;
+        if(isEmpty()){
+            return null;
+        }else{
+            E value = data[t];
+            data[t] = null;
+            t--;
+            return value;
+        }
     }
 
     /**
