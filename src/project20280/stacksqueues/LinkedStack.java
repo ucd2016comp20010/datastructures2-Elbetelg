fb6ea385432b32dfdef5b1b6c29c6d6c33f9e67d
elbetelg <elbetelgetu2020@gmail.com>
2026-02-06 15:31:51 +0000

    Completed LinkedStack  checkParentheses and convertToBinary implementation for Q4 and Q5.

--- src/project20280/stacksqueues/LinkedStack.java
+++ src/project20280/stacksqueues/LinkedStack.java
@@ -7,9 +7,6 @@ public class LinkedStack<E> implements Stack<E> {
 
     DoublyLinkedList<E> ll;
 
-    public static void main(String[] args) {
-    }
-
     public LinkedStack() {
         // TODO
         ll = new DoublyLinkedList<>();
@@ -50,4 +47,70 @@ public class LinkedStack<E> implements Stack<E> {
     public String toString() {
         return ll.toString();
     }
-}
+
+    static String convertToBinary(long dec_num) {
+        if (dec_num == 0) return "0";
+
+        Stack<Long> s = new LinkedStack<>();
+
+        while (dec_num > 0) {
+            s.push(dec_num % 2);
+            dec_num /= 2;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!s.isEmpty()) {
+            sb.append(s.pop());
+        }
+        return sb.toString();
+    }
+
+    public static boolean checkParentheses(String in) {
+        Stack<Character> stack = new LinkedStack<>();
+
+        for (char c : in.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+
+            else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if (!isMatch(top, c)) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static boolean isMatch(char open, char close) {
+        return (open == '(' && close == ')') ||
+                (open == '[' && close == ']') ||
+                (open == '{' && close == '}');
+    }
+
+    public static void main(String[] args) {
+        String[] inputs = {
+                "[]]()()",           // false
+                "c[d]",              // true
+                "a{b[c]d}e",         // true
+                "a{b(c]d}e",         // false
+                "a[b{c}d]e}",        // false
+                "a{b(c) ",           // false
+                "][]][][[]][]][][[[", // false
+                "(((abc))((d)))))"   // false
+        };
+
+        System.out.println("Testing Parentheses");
+        for (String input : inputs) {
+
+            boolean isBalanced = checkParentheses(input);
+            System.out.println("isBalanced " + (isBalanced ? " yes! " : " no!  ") + input);
+        }
+
+        System.out.println("\nTesting Binary Conversion");
+        System.out.println("23 in Binary: " + convertToBinary(23));
+    }
+ }
+
